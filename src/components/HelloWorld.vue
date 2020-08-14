7abe31f2d336fcdbf5c49d5b988c6f6304315667
r000909 <r000909@pc-000909-6.local>
2020-08-14 19:05:20 +0900

    [add] first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,90 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <table class="ranking-table">
+      <thead>
+        <tr>
+          <th>RANK</th>
+          <th>NAME</th>
+          <th>SCORE</th>
+        </tr>
+      </thead>
+      <transition-group tag="tbody">
+        <tr v-for="(post, index) in allScoreData" :key="post.id">
+          <td :class="{newRecord: post.isNew}">{{index + 1}} 位</td>
+          <td :class="{newRecord: post.isNew}">{{post.name}}</td>
+          <td :class="{newRecord: post.isNew}">{{post.score}}</td>
+        </tr>
+      </transition-group>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      allScoreData: [], // {id: Number, name: String, score: Number, isNew: Boolean}
+    }
+  },
+  mounted() {
+    const socket = new WebSocket('ws://localhost:5001');
+
+    socket.onmessage = event => {
+      console.dir(event.data)
+      this.addData(JSON.parse(event.data));
+    }
+  },
+  methods: {
+    addData(data) {
+      // 既存のスコアのisNewをfalseにする (= 新規レコードのフラグを外す)
+      const list = this.allScoreData.map(val => {
+        return {
+          id: val.id,
+          name: val.name,
+          score: val.score,
+          isNew: false
+        };
+      });
+
+      list.push({
+        id: list.length + 1,
+        name: data.name,
+        score: Number(data.score),
+        isNew: true
+      });
+
+      // 並び順をスコアの降順に変更
+      this.allScoreData = list.sort((a,b) => (a.score > b.score ? -1 : 1))
+    },
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.ranking-table {
+  margin: auto;
+  width: 50vw;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.newRecord {
+  background-color: #ff7f50;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+/* animation */
+.v-leave-active,
+.v-enter-active {
+  transition: opacity .5s, transform .5s;
 }
-a {
-  color: #42b983;
+.v-leave-to,
+.v-enter {
+  opacity: 0;
+  transform: translateX(200px);
+}
+.v-leave,
+.v-enter-to {
+  opacity: 1;
+}
+.v-move {
+  transition: transform .5s;
 }
 </style>
